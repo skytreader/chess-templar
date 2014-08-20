610f412dab00ee2e5dc963f800f52469a3088487
Chad Estioco <chadestioco@gmail.com>
2014-08-20 18:34:33 +0800

    Revised the interface for ChessPiece.

--- src/main/java/net/skytreader/kode/chesstemplar/pieces/ChessPiece.java
+++ src/main/java/net/skytreader/kode/chesstemplar/pieces/ChessPiece.java
@@ -1,12 +1,17 @@
 package net.skytreader.kode.chesstemplar.pieces;
 
+import java.awt.Point;
+
 public interface ChessPiece{
     /**
-    Return the new position of this piece, in chess algebraic notation.
+    Get all the legal moves of the piece, given its current position.
+
+    @param r
+        An integer describing the current row of the piece. Range 0-7. In
+        algebraic notation, this is the <i>rank</i>.
+    @param c
+        An integer describing the current column of the piece. Range 0-7. In
+        algebraic notation this is the <i>file</i>.
     */
-    public String move(char col, int row);
-    /**
-    Return the current position of this piece, in chess algebraic notation.
-    */
-    public String getCurrentPosition();
+    public Point[] getLegalMoves(int r, int c);
 }
