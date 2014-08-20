74f621a56bcd9f0790aa61cddd829145843eec3f
Chad Estioco <chadestioco@gmail.com>
2014-08-20 15:13:16 +0800

    Initial test structure.
    Also updated the JUnit version used to 4.11; for some reason, Maven's default
    does not work for my local environment.

---
+++ src/test/java/net/skytreader/kode/chesstemplar/utils/NotationParserTest.java
@@ -0,0 +1,12 @@
+package net.skytreader.kode.chesstemplar.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NotationParserTest{
+    
+    @Test
+    public void testNotations(){
+        Assert.assertTrue(true);
+    }
+}
