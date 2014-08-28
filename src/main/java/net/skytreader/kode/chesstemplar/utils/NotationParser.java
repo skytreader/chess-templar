cb6c6ba33e9969b90f0ef4ca53d849ba4e65bce1
Chad Estioco <chadestioco@gmail.com>
2014-08-28 18:37:34 +0800

    Passing regex for capture notation.

--- src/main/java/net/skytreader/kode/chesstemplar/utils/NotationParser.java
+++ src/main/java/net/skytreader/kode/chesstemplar/utils/NotationParser.java
@@ -7,6 +7,9 @@ import java.util.regex.Pattern;
 /**
 We would be parsing standard algebraic notation as required by FIDE.
 
+Our current reference is: http://en.wikipedia.org/wiki/Algebraic_notation_(chess)
+but I would like something more official (like from FIDE).
+
 This uses the English notation for pieces that is,
     - K for king
     - Q for queen
@@ -24,14 +27,28 @@ for "Bishop to e5", but the following, a common notation for a chess turn, is no
     e4 e5
 */
 public class NotationParser{
-    private final String LEGAL_COLUMNS = "abcdefgh";
-    private final String NOTATION_REGEX = "[KQRBN]?[abcdefgh][1-8]";
+    private static final String LEGAL_COLUMNS = "abcdefgh";
+    private static final String LEGAL_PIECES = "KQRBN";
+    private static final String NOTATION_REGEX = "[" + LEGAL_PIECES + "]?[" + LEGAL_COLUMNS + "][1-8]";
+    private static final String CAPTURE_REGEX = "([" + LEGAL_PIECES + "]x)?[" + LEGAL_COLUMNS + "][1-8]";
+
+    private static final Pattern[] LEGAL_NOTATION = {
+        Pattern.compile(NOTATION_REGEX),
+        Pattern.compile(CAPTURE_REGEX),
+    };
     
     /**
     Returns true if the given string is valid chess algebraic notation.
     */
     public static boolean isValid(String s){
-        return false;
+        boolean isValid = false;
+        for(Pattern p : LEGAL_NOTATION){
+            if(p.matcher(s).matches()){
+                isValid = true;
+                break;
+            }
+        }
+        return isValid;
     }
     
     public static Point parse(String s){
