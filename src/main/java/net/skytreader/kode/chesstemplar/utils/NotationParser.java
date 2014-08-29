43fa354203f940fb7b6103609a587f31c7bfed13
Chad Estioco <chadestioco@gmail.com>
2014-08-29 15:06:26 +0800

    Complete regex set for NotationParser.

--- src/main/java/net/skytreader/kode/chesstemplar/utils/NotationParser.java
+++ src/main/java/net/skytreader/kode/chesstemplar/utils/NotationParser.java
@@ -31,15 +31,17 @@ public class NotationParser{
     private static final String LEGAL_PIECES = "KQRBN";
     private static final String NOTATION_REGEX = "[" + LEGAL_PIECES + "]?[" + LEGAL_COLUMNS + "][1-8]";
     private static final String CAPTURE_REGEX = "[" + LEGAL_PIECES + LEGAL_COLUMNS + "]x[" + LEGAL_COLUMNS + "][1-8]";
-    private static final String DISAMBIGUATION_REGEX = "[" + LEGAL_PIECES + "]([" + LEGAL_COLUMNS + "]|[1-8])" + LEGAL_COLUMNS + "[1-8]";
+    private static final String DISAMBIGUATION_REGEX = "[" + LEGAL_PIECES + "]([" + LEGAL_COLUMNS + "]|[1-8])[" + LEGAL_COLUMNS + "][1-8]";
     // FIXME A tighter set?
     private static final String PAWN_PROMO_REGEX = "[" + LEGAL_COLUMNS + "][1-8]Q";
+    private static final String CASTLING_REGEX = "(0-0|0-0-0)";
 
     private static final Pattern[] LEGAL_NOTATION = {
         Pattern.compile(NOTATION_REGEX),
         Pattern.compile(CAPTURE_REGEX),
         Pattern.compile(DISAMBIGUATION_REGEX),
         Pattern.compile(PAWN_PROMO_REGEX),
+        Pattern.compile(CASTLING_REGEX),
     };
     
     /**
