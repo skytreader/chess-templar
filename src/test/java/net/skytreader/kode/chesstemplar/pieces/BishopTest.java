e88995efdb7e8400c1d80fb91fda33e09c36f965
Chad Estioco <chadestioco@gmail.com>
2014-09-09 17:34:18 +0800

    Adding initial unittests for Bishop getLegalMoves.

--- src/test/java/net/skytreader/kode/chesstemplar/pieces/BishopTest.java
+++ src/test/java/net/skytreader/kode/chesstemplar/pieces/BishopTest.java
@@ -4,18 +4,25 @@ import java.awt.Point;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 import net.skytreader.kode.chesstemplar.Board;
 import net.skytreader.kode.chesstemplar.GridBoard;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import org.junit.rules.ExpectedException;
 
 public class BishopTest{
     
     private Bishop whiteBishop;
     private Bishop blackBishop;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
     
     @Before
     public void setUp(){
@@ -78,5 +85,33 @@ public class BishopTest{
 
         // Test that black does not equal white (transitive with testEqualsWhite).
         Assert.assertFalse(blackBishop.equals(whiteBishop));
+    }
+    
+    /**
+    Test the legal moves allowed for a Bishop in the game's initial configuration.
+
+    The legal moves: nothing.
+    */
+    @Test
+    public void testInitialLegalMoves(){
+        try{
+            Board testBoard = new GridBoard();
+
+            HashSet<Point> legalMoves = new HashSet<Point>();
+            Set<Point> fromBishop = blackBishop.getLegalMoves(0, 2, testBoard);
+            Assert.assertEquals(legalMoves, fromBishop);
+        } catch(NotMeException nme){
+            Assert.fail("NotMeException thrown while testing legal moves.");
+            nme.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testNotMe() throws NotMeException{
+        exception.expect(NotMeException.class);
+        Board testBoard = new GridBoard();
+
+        // Use this Bishop to get the moves of a pawn
+        whiteBishop.getLegalMoves(0, 1, testBoard);
     }
 }
