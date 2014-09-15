6dcecc61c583866b40336c744737a320e9beb854
Chad Estioco <chadestioco@gmail.com>
2014-09-15 18:23:46 +0800

    Modifications to BlankBoard and unit-test spec.

---
+++ src/test/java/net/skytreader/kode/chesstemplar/BlankBoardTest.java
@@ -0,0 +1,36 @@
+package net.skytreader.kode.chesstemplar;
+
+import java.awt.Point;
+
+import net.skytreader.kode.chesstemplar.pieces.ChessPiece;
+import net.skytreader.kode.chesstemplar.pieces.Pawn;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlankBoardTest{
+    
+    private BlankBoard board;
+
+    @Before
+    public void setUp(){
+        board = new BlankBoard();
+    }
+
+    @Test
+    public void testInitialState(){
+        for(int row = 0; row < 8; row++){
+            for(int col = 0; col < 8; col++){
+                Assert.assertNull(board.getPieceAt(row, col));
+            }
+        }
+    }
+
+    @Test
+    public void testAddPiece(){
+        Pawn whitePawn = new Pawn(true);
+        board.addPiece(whitePawn, 4, 4);
+        Assert.assertEquals(whitePawn, board.getPieceAt(4, 4));
+    }
+}
