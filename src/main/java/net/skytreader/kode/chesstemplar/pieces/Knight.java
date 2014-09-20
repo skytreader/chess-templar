0ace89e964d817fc3f1fb95e0f43801eb3a4d6b0
Chad Estioco <chadestioco@gmail.com>
2014-09-21 03:59:00 +0800

    Implemented getLegalMoves for Knight. Tests still failing tho.

--- src/main/java/net/skytreader/kode/chesstemplar/pieces/Knight.java
+++ src/main/java/net/skytreader/kode/chesstemplar/pieces/Knight.java
@@ -14,6 +14,10 @@ public class Knight extends ChessPiece{
         this.pieceName = "KNIGHT";
     }
 
+    private boolean movesetFilter(Point p){
+        return 0 <= p.x && p.x < 8 && 0<= p.y && p.y <= 8;
+    }
+
     @Override
     public Set<Point> getLegalMoves(int r, int c, Board b) throws NotMeException{
         ChessPiece indicatedPiece = b.getPieceAt(r, c);
@@ -21,8 +25,21 @@ public class Knight extends ChessPiece{
             throw new NotMeException("Attempted to move piece " + indicatedPiece.toString()
              + " using " + this.toString());
         }
-        HashSet<Point> legalMoves = new HashSet<Point>();
+        HashSet<Point> moveSet = new HashSet<Point>();
 
-        return legalMoves;
+        Point[] allAround = {new Point(r - 2, c - 1), new Point(r - 1, c - 2),
+          new Point(r - 2, c + 1), new Point(r - 1, c + 2),
+          new Point(r + 1, c - 2), new Point(r + 2, c - 1),
+          new Point(r + 1, c + 2), new Point(r + 2, c + 1)};
+
+        for(Point p : allAround){
+            ChessPiece thisPiece = b.getPieceAt(p.x, p.y);
+            if((thisPiece == null || thisPiece.isWhite() ^ this.isWhite())
+              && movesetFilter(p)){
+                moveSet.add(p);
+            }
+        }
+
+        return moveSet;
     }
 }
