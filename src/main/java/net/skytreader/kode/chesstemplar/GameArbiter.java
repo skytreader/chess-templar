7f9b8250374c377cdf2de68a94fd19bdadc39ffa
Chad Estioco <chadestioco@gmail.com>
2014-10-02 17:56:24 +0800

    Initial commit of GameArbiter class.

---
+++ src/main/java/net/skytreader/kode/chesstemplar/GameArbiter.java
@@ -0,0 +1,48 @@
+package net.skytreader.kode.chesstemplar;
+
+/**
+The GameArbiter imposes the rules of Chess. An arbiter is tied to a particular
+game. You can't make an Arbiter judge on multiple boards at a time!
+
+@author Chad Estioco
+*/
+public class GameArbiter{
+    
+    private Board board;
+    private boolean whiteKingMoved;
+    private boolean whiteKingsideRookMoved;
+    private boolean whiteQueensideRookMoved;
+    private boolean blackKingMoved;
+    private boolean blackKingsideRookMoved;
+    private boolean blackQueensideRookMoved;
+
+    public GameArbiter(Board b){
+        board = b;
+        whiteKingMoved = false;
+        whiteKingsideRookMoved = false;
+        whiteQueensideRookMoced = false;
+        blackKingMoved = false;
+        blackKingsideRookMoved = false;
+        blackQueensideRookMoved = false;
+    }
+
+    public boolean canWhiteKingCastle(){
+        return false;
+    }
+
+    public boolean canBlackKingCastle(){
+        return false;
+    }
+
+    public boolean isWhiteKingChecked(){
+        return false;
+    }
+
+    public boolean isBlackKingChecked(){
+        return false;
+    }
+
+    public boolean isEndgame(){
+        return false;
+    }
+}
