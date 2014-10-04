5e7853be1b1293d0dfd388912c33aea7e921291f
Chad Estioco <chadestioco@gmail.com>
2014-10-04 17:20:18 +0800

    Adding InvalidStateException.
    This may be useful for GameArbiter whenever it is asked about a certain game
    configuration then suddenly it does not know what to do because, guess what,
    the game is in an invalid state.

---
+++ src/main/java/net/skytreader/kode/chesstemplar/exceptions/InvalidStateException.java
@@ -0,0 +1,31 @@
+package net.skytreader.kode.chesstemplar.exceptions;
+
+/**
+This is exception is thrown whenever, for some reason, it is detected that a
+chess game has reached an invalid state.
+
+Possible invalid states include but are not limited to:
+<ul>
+    <li>The number of Kings in the board is not equal to 2.</li>
+    <li>There are more than eight pawns for a particular color.</li>
+</ul>
+
+@author Chad Estioco
+*/
+public class InvalidStateException extends Exception{
+    public InvalidStateException(){
+        super();
+    }
+    public InvalidStateException(String message){
+        super(message);
+    }
+    public InvalidStateException(String message, Throwable cause){
+        super(message, cause);
+    }
+    public InvalidStateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace){
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    public InvalidStateException(Throwable cause){
+        super(cause);
+    }
+}
