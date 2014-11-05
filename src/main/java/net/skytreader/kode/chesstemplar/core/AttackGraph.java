8d10e126daba90eb1102fb7125bae44614035719
Chad Estioco <chadestioco@gmail.com>
2014-11-05 16:23:43 +0800

    Preliminary commit for AttackGraph.

---
+++ src/main/java/net/skytreader/kode/chesstemplar/core/AttackGraph.java
@@ -0,0 +1,30 @@
+package net.skytreader.kode.chesstemplar.core;
+
+import net.skytreader.kode.chesstemplar.Board;
+
+import net.skytreader.kode.chesstemplar.pieces.ChessPiece;
+
+/**
+An attack graph represents the attacking relationship between pieces in a
+Chess board. The graph is acyclic and the nodes represent the pieces in the
+game. An edge coming from node A to node B means that piece B is under attack
+by piece A.
+
+@author Chad Estioco
+*/
+public class AttackGraph{
+    
+    private Board board;
+
+    /**
+    Construct an attack graph from the current configuration of the Board b.
+
+    @param b
+    */
+    public class AttackGraph(Board b){
+        board = b;
+    }
+
+    public void move(int r1, int c1, int r2, int c2){
+    }
+}
---
+++ src/test/java/net/skytreader/kode/chesstemplar/core/AttackGraphTest.java
@@ -0,0 +1,12 @@
+package net.skytreader.kode.chesstemplar.core;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AttackGraphTest{
+    
+    @Before 
+    public class setUp(){
+    }
+}
