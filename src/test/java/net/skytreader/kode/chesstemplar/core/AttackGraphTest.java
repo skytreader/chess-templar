06c053fba64888394c5db5af1aace3335d9d5e62
Chad Estioco <chadestioco@gmail.com>
2014-11-13 02:37:01 +0800

    Very minor unit-test set-up.
    boo Palaging 'Lang Dial Tone. boo.

--- src/test/java/net/skytreader/kode/chesstemplar/core/AttackGraphTest.java
+++ src/test/java/net/skytreader/kode/chesstemplar/core/AttackGraphTest.java
@@ -1,12 +1,20 @@
 package net.skytreader.kode.chesstemplar.core;
 
+import net.skytreader.kode.chesstemplar.BlankBoard;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class AttackGraphTest{
     
+    private BlankBoard testBoard;
+    private AttackGraph testGraph;
+
     @Before 
     public void setUp(){
+        testBoard = new BlankBoard();
+        // TODO Find a good configuration
+        testGraph = new AttackGraph(testBoard);
     }
 }
