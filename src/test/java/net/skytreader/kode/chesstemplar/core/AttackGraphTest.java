5a7e1e1a37047a4852e6bdaa999ea0f7bd1e97d8
Chad Estioco <chadestioco@gmail.com>
2014-11-14 22:22:06 +0800

    Example test structure for AttackGraph.

--- src/test/java/net/skytreader/kode/chesstemplar/core/AttackGraphTest.java
+++ src/test/java/net/skytreader/kode/chesstemplar/core/AttackGraphTest.java
@@ -1,7 +1,10 @@
 package net.skytreader.kode.chesstemplar.core;
 
+import java.awt.Point;
+
 import net.skytreader.kode.chesstemplar.BlankBoard;
 
+import net.skytreader.kode.chesstemplar.pieces.Bishop;
 import net.skytreader.kode.chesstemplar.pieces.King;
 import net.skytreader.kode.chesstemplar.pieces.Pawn;
 import net.skytreader.kode.chesstemplar.pieces.Rook;
@@ -26,6 +29,15 @@ public class AttackGraphTest{
         testBoard.addPiece(new Rook(false), 3, 1);
         testBoard.addPiece(new King(false), 4, 6);
         testBoard.addPiece(new Pawn(false), 5, 7);
+        // This is an additional
+        testBoard.addPiece(new Bishop(false), 5, 1);
         testGraph = new AttackGraph(testBoard);
+    }
+
+    @Test
+    public void testIsAttacking(){
+        // Same color does not attack
+        Assert.assertFalse(testGraph.isAttacking(new Point(2, 1), new Point(3, 0)));
+        Assert.assertFalse(testGraph.isAttacking(new Point(5, 7), new Point(4, 6)));
     }
 }
