486a580d446ef958f12b88da57cf0d0161f6e6d7
Chad Estioco <chadestioco@gmail.com>
2014-12-09 22:54:05 +0800

    No longer checking if observedBoard hasChanged.
    Also removing debugging printlns and it looks like we have a lot of null
    pointers again!

--- src/main/java/net/skytreader/kode/chesstemplar/core/AttackGraph.java
+++ src/main/java/net/skytreader/kode/chesstemplar/core/AttackGraph.java
@@ -155,7 +155,6 @@ public class AttackGraph implements Observer{
       - attacker and target are not equal
     */
     private void updateAttackGraph(Point attacker, Point target){
-        System.out.println("updateAttackGraph");
         Set<Point> theAttacked = attackGraph.get(attacker);
         theAttacked.add(target);
         attackGraph.put(attacker, theAttacked);
@@ -163,8 +162,7 @@ public class AttackGraph implements Observer{
 
     @Override
     public void update(Observable o, Object arg){
-        if(observedBoard.equals(o) && observedBoard.hasChanged()){
-            System.out.println("update Updating attack graph...");
+        if(observedBoard.equals(o)){
             // Only do something if the update comes from the board we are
             // observing.
 
@@ -196,7 +194,6 @@ public class AttackGraph implements Observer{
                 for(Point pos : piecePos){
                     ChessPiece cp = observedBoard.getPieceAt(pos.x, pos.y);
                     Set<Point> moves = cp.getMoves(pos.x, pos.y, observedBoard);
-                    System.out.println("update " + cp + " is attacking " + movedPiece);
                     
                     if(moves.contains(moveDesc[1])){
                         updateAttackGraph(pos, moveDesc[1]);
