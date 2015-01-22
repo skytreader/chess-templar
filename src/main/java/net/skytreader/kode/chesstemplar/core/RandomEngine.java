00d72a7034027826451e96ff89242a93ff131600
Chad Estioco <chadestioco@gmail.com>
2015-01-23 05:09:13 +0800

    Implementation for getMove of RandomEngine.

---
+++ src/main/java/net/skytreader/kode/chesstemplar/core/RandomEngine.java
@@ -0,0 +1,28 @@
+package net.skytreader.kode.chesstemplar.core;
+
+import java.awt.Point;
+
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Set;
+
+import net.skytreader.kode.chesstemplar.GameArbiter;
+
+/**
+Just does random moves. For illustrative purposes only.
+*/
+public class RandomEngine extends AIEngine{
+    private Random randomChooser;
+    
+    public RandomEngine(GameArbiter ga){
+        super(ga);
+        randomChooser = new Random();
+    }
+    
+    @Override
+    public Point[] getMove(){
+        LinkedList<Point[]> nextMoves = new LinkedList<Point[]>(getAllLegalMoves());
+        int randomMoveIndex = randomChooser.nextInt(nextMoves.size());
+        return nextMoves.get(randomMoveIndex);
+    }
+}
