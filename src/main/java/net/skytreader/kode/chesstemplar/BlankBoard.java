2193514d0b9d35492fce7cd2fb765ba5e2e6c816
Chad Estioco <chadestioco@gmail.com>
2017-07-21 23:27:09 +0800

    Docstring update. [ci skip]

--- src/main/java/net/skytreader/kode/chesstemplar/BlankBoard.java
+++ src/main/java/net/skytreader/kode/chesstemplar/BlankBoard.java
@@ -7,8 +7,7 @@ import net.skytreader.kode.chesstemplar.pieces.ChessPiece;
 
 /**
 Grid implementation of Board that <i>does not</i> place anything on Board upon
-initialization. Consequently, this class provides an additional method, addPiece
-to add a ChessPiece in the blank board.
+initialization.
 
 The purpose of this class is for quick scenario set-ups in tests. Use only for
 tests; we might optimize GridBoard, but we sure as hell won't optimize BlankBoard.
